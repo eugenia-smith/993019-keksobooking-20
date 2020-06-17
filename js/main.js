64a12240e96f025d550c57d14c8a21bb3467dc2e
eugenia-smith <ev-genius4@mail.ru>
2020-06-18 00:41:55 +0400

    Правка по замечаниям линтера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 var locationLimits = {
   xmin: 130,
   xmax: 630,
@@ -6,70 +6,70 @@ var locationLimits = {
   ymax: 630
 };
 
-//массивы данных
+// массивы данных
 var accommodationTypes = ['palace', 'flat', 'house', 'bungalo'];
 var accommodationFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var checkinTime = ['12:00', '13:00', '14:00'];
 var checkoutTime = ['12:00', '13:00', '14:00'];
 var photosData = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 
-//генерирует случайные числа
+// генерирует случайные числа
 var getRandomInt = function (min, max) {
   var rand = Math.random() * (max - min + 1) + min;
   return Math.floor(rand);
-}
+};
 
 
-//получает случайные элемент массива
-var getRandomArrayElement = function(arr) {
+// получает случайные элемент массива
+var getRandomArrayElement = function (arr) {
   var rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
-}
+};
 
 
-//никто не знает, будет ли эта функция единственным источником данных
-//id остается на откуп внешнему коду
+// никто не знает, будет ли эта функция единственным источником данных
+// id остается на откуп внешнему коду
 var getAuthor = function (id) {
- var author = {
-  avatar: 'img/avatars/user0' + id + '.png',
- };
+  var author = {
+    avatar: 'img/avatars/user0' + id + '.png',
+  };
   return author;
-}
+};
 
-//генерирует позицию
+// генерирует позицию
 var getLocation = function (limits) {
- var location = {};
+  var location = {};
   location.x = getRandomInt(limits.xmin, limits.xmax);
   location.y = getRandomInt(limits.ymin, limits.ymax);
-return location;
-}
+  return location;
+};
 
-//формирует список опций
+// формирует список опций
 var getFeatures = function () {
   var featuresArr = [];
-  for (var i = 0; i < accommodationFeatures.length; i++){
-    if (getRandomInt(1, 10) > 5){
+  for (var i = 0; i < accommodationFeatures.length; i++) {
+    if (getRandomInt(1, 10) > 5) {
       featuresArr.push(accommodationFeatures[i]);
     }
   }
   return featuresArr;
-}
+};
 
-//подбирает фотографии
-var getPhoto = function (){
+// подбирает фотографии
+var getPhoto = function () {
   var photosArr = [];
   for (var i = 0; i < photosData.length; i++) {
     if (getRandomInt(1, 6) > 3) {
-    photosArr.push(photosData[i]);
-   }
+      photosArr.push(photosData[i]);
+    }
   }
   return photosArr;
-}
+};
 
-//формирует карточку предложения
+// формирует карточку предложения
 var getOffer = function (id, location) {
   var offer = {};
   offer.title = 'Объявление ' + id;
@@ -84,27 +84,27 @@ var getOffer = function (id, location) {
   offer.description = 'Описание ' + id;
   offer.photos = getPhoto();
   return offer;
-}
+};
 
-//создает 1 объект
+// создает 1 объект
 var getPin = function (id) {
   var pin = {};
   pin.author = getAuthor(id);
   pin.location = getLocation(locationLimits);
   pin.offer = getOffer(id, pin.location);
   return pin;
-}
+};
 
-//создает массив пинов
+// создает массив пинов
 var getPins = function () {
   var arrPins = [];
   for (var i = 1; i <= 8; i++) {
-    arrPins.push(getPin(i))
+    arrPins.push(getPin(i));
   }
   return arrPins;
-}
+};
 
-//удаляет класс
+// удаляет класс
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
